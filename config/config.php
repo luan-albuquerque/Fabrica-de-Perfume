95843952c614a4b4ec28bf374648b00cc5b0002d
luan <luan.santos6605@gmail.com>
2021-04-01 20:55:45 -0400

    Criando Dispachante

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 $_pasta_interna = "";
 
-# DIRPAGE - LOCALIZAÇÃO DE DA PAGINA
+
 define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$_pasta_interna}");
 
-# DIRREQ - LOCALIZAÇÃO DOS ARQUIVOS
+
 if(substr($_SERVER['DOCUMENT_ROOT'],-1) == "/"){
          define('DIRREQ', "{$_SERVER['DOCUMENT_ROOT']}{$_pasta_interna}");
 }else{
