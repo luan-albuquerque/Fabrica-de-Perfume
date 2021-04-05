3ce4e4b325e5eb3e48adc899638c3b2c41142d92
Luan Albuquerque <luan.santos6605@gmail.com>
2021-04-05 01:48:51 -0400

    Controller Home contruçao de layout e execuçao de funçoes

--- app/controllers/ControllerHome.php
+++ app/controllers/ControllerHome.php
@@ -1,10 +1,72 @@
 <?php
 namespace App\Controllers;
 
-class ControllerHome{
+use App\models\ClassConexao;
+use App\models\ModelFragancia;
+use Src\Classes\ClassRender;
 
-    public function __construct()
-    {
-        
-    }
+class ControllerHome extends ModelFragancia{
+    
+public function __construct()
+{   
+       $Render = new ClassRender;
+       $Render->setDir("home");
+       $Render->setTitle("Pagina Inicial");
+       $Render->setDescription("Pagina de Inicio");
+       $Render->setKeyWords("PgInicial");
+       $Render->renderLayout();
+    
+}
+
+public function CarregarTabela(){
+    $result = $this->ListarFrag();
+
+    echo " <table cellspacing='0' cellpadding='4' border='0' id='ctl00_Body_grvChamados' style='color:#333333;width:100%;border-collapse:collapse;'>
+	
+        <!--DEF DE COLUNAS -->
+         	<tr style='color:White;background-color:#71C39A;font-weight:bold;'>
+			<th scope='col'>ID</th>
+            <th scope='col'>Registro</th>
+            <th scope='col'>Fragancia</th>
+            <th scope='col'>Volume Fragância</th>
+            <th scope='col'>Volume Agua</th> 
+            <th scope='col'>Volume Alcool</th> 
+            </tr>
+
+        <!--FIM DEF DE COLUNAS-->
+        <!-- TR DE VALORES ACRESCENTA -->
+    ";
+
+    foreach($result as $dados){
+    echo "
+        <tr align='center' style='background-color:#E3EAEB;'>
+			
+            <td>$dados[ID]</td>
+            <td><STRONG>$dados[REG]</STRONG></td>
+            <td>$dados[ID_F]</td>
+            <td>$dados[VF]</td>
+            <td>$dados[VA]</td>
+            <td>$dados[VAL]</td>
+
+            <td align='center' style='width:20px;'>
+            <a href='easy.pdf' target='blank'  class='btn-action glyphicons eye_open btn-info'>
+              <i></i>
+             </a>
+            </td>
+	       </tr>
+
+            ";
+        }
+           echo "
+        <!--FIM DO TR DE ADIÇÃO-->
+
+
+	       </table>";
+
+
+
+}
+
+
+
 }
