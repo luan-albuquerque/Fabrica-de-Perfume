e5c383f97ae30cfc4918acc965fc303c0fbfd58c
Luan Albuquerque <luan.santos6605@gmail.com>
2021-04-07 12:34:45 -0400

    Novo model para trabalhar apenas com as tabelas de fragancias

--- app/models/ModelFragancia.php
+++ app/models/ModelFragancia.php
@@ -1,47 +1,34 @@
 <?php
-
-namespace App\Models;
+namespace App\models;
 
 use App\models\ClassConexao;
 
-class ModelFragancia extends ClassConexao
-{
+class ModelFragancia extends ClassConexao{
 
-    private $db;
 
-    protected function cadastrarFrag($servdesc, $servcgl)
-    {
-        $this->db = $this->connectionMysql()
-            ->prepare("INSERT INTO F_PERFUME VALUES(null,:dtreg,:key_frag,:volume)");
-        $this->db->bindParam(":dtreg", date("Y-m-d"), \PDO::PARAM_STR);
-        $this->db->bindParam(":key_frag", $key_frag, \PDO::PARAM_INT);
-        $this->db->bindParam(":volume", $key_frag, \PDO::PARAM_STR);
-        $this->db->execute();
-    }
+protected function ListarFrag(){
 
- protected function ListarFrag()
-    {
-       $Bfetch = $this->db = $this->connectionMysql()
-            ->prepare("SELECT * FROM perfume");
-        $Bfetch->execute();
-        $I = 0;
-        while($Fetch = $Bfetch->fetch(\PDO::FETCH_ASSOC)){
-            $ArrayList[$I] = [
-            'COD' => $Fetch['id'],
-            'REG' => $Fetch['dtregistro'],
-             'ID_F' =>  $Fetch['id_frag'],
-             'VF'  => $Fetch['volumeFra'],
-             'VA'  => $Fetch['volumeAgua'],
-             'VAL' => $Fetch['volumeAlcool']
-            ];
-            $I++;
-        
-        }
-          if(isset($ArrayList)){
-              return $ArrayList;
-          } else{
-              echo "Erro Retornar Lista";
-          }
-    
-        }
+   $BFetch = $this->connectionMysql()
+   ->prepare("SELECT * FROM fragancia");
+   $BFetch->execute();
+   $I =0;
+   while($Fetch = $BFetch->Fetch(\PDO::FETCH_ASSOC)){
+   $ArrayList[$I] = [
+       'COD' => $Fetch['id'],
+       'DESC' => $Fetch['descr']
+    ]; 
+   $I++;
+   if(isset($ArrayList)){
+       return $ArrayList;
+   }else{
+       echo "Erro ao Listar Conteúdo";
+   }
 }
+
+}
+
+
+
+}
+
+?>
