cde25e0ce712bec3b247518e9bae561880010c03
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2021-04-08 01:40:42 -0300

    Criando controllers do ingredientes

---
+++ app/controllers/ControllerEstAgua.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\controllers;
+
+use App\models\ModelEstAgua;
+
+class ControllerEstAgua extends ModelEstAgua{
+
+
+
+    
+}
---
+++ app/controllers/ControllerEstAlcool.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\controllers;
+
+use App\models\ModelEstAlcool;
+
+class ControllerEstAlcool extends ModelEstAlcool{
+
+
+
+    
+}
