aface88d86fe80cb6c4b1f14ef5fe5ddd385e5fa
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2021-04-09 00:57:13 -0300

    Finalizando questionamento de dados

--- public/js/meujs.js
+++ public/js/meujs.js
@@ -1,48 +1,51 @@
 
-    $(window).load(function() {
-        var perfume,tipo;
+$(window).load(function () {
+    var perfume, tipo;
 
-        $('button[name=preparar]').click(function() {
-            perfume = document.getElementById("perfume").value;
-            parcela1 = perfume / 2;
-            parcela2 = parcela1 / 2;
-            tipo = document.getElementById("tipo_v").value;
+    $('button[name=preparar]').click(function () {
+        perfume = document.getElementById("perfume").value;
+        parcela1 = perfume / 2;
+        parcela2 = parcela1 / 2;
+        tipo = document.getElementById("tipo_v").value;
 
-if(tipo=='1'){
-    document.getElementById("frag").value = (parcela2 * 1000);
-    document.getElementById("agua").value = (parcela1 * 1000);
-    document.getElementById("alcool").value = (parcela2 * 1000);
-    
-} else{
-    document.getElementById("frag").value = parcela2;
-    document.getElementById("agua").value = parcela1;
-    document.getElementById("alcool").value = parcela2;
+        if (tipo == '1') {
+            document.getElementById("frag").value = (parcela2 * 1000);
+            document.getElementById("agua").value = (parcela1 * 1000);
+            document.getElementById("alcool").value = (parcela2 * 1000);
 
-}
+        } else {
+            document.getElementById("frag").value = parcela2;
+            document.getElementById("agua").value = parcela1;
+            document.getElementById("alcool").value = parcela2;
 
-        });
+        }
 
     });
 
+});
 
-function req_p(){
-   var perfume,frag,agua,alcool;
 
-     perfume = document.getElementById("perfume").value;
-     frag = document.getElementById("frag").value;
-     agua = document.getElementById("agua").value;
-     alcool = document.getElementById("alcool").value;
-    
-    addEventListener('submit', function (e) {
-       
-        if((agua+frag+alcool)!='1000'){
-        alert("Valores acima do esperado.");
-        e.preventDefault();
-    
-    }else{
+function req_p() {
+    var perfume, frag, agua, alcool, soma, tipo;
 
-        alert("Funcionou!!!");
+    perfume = parseInt(document.getElementById("perfume").value);
+    frag = document.getElementById("frag").value;
+    agua = document.getElementById("agua").value;
+    alcool = document.getElementById("alcool").value;
+    soma = parseInt(frag) + parseInt(agua) + parseInt(alcool);
+    tipo = document.getElementById("tipo_v").value;
+
+    if (tipo == '1') { perfume = perfume * 1000 }
+
+    if (parseInt(soma) != parseInt(perfume)) {
+        alert("Valores Incopativeis!!!");
+        addEventListener('submit', function (e) { e.preventDefault(); });
+         req_p();
+    } else {
+        alert("Cadastrar com Sucesso.");
+        addEventListener('submit', function (e) { e.initEvent(); });
     }
 
-    });
+
+
 };
