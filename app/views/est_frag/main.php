2dff10f8273ff02cadacd526b0235ae286dc7e4f
Luan Albuquerque <luan.santos6605@gmail.com>
2021-04-12 01:52:13 -0400

    Arrumando estrutura para receber os dados

---
+++ app/views/est_frag/main.php
@@ -0,0 +1,10 @@
+<div id="content">
+    <h3 class="glyphicons table"><i></i>Estoque de Fragancia</h3>
+<?php
+
+$TABLE = new \App\controllers\ControllerEstFrag;
+$TABLE->TabelaDeEstFrag();
+
+?>
+
+</div>
